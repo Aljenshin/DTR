e53a8df355ccdebdce3b5ff3b52a9f7d27f468d2
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-01-25 15:30:26 +0800

    login wip

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/oauth/login', function () {
+    return view('log-in.log-in');
+});
+Route::get('/oauth/logout', function () {
+    return view('log-in.logout');
+});
+Route::get('/oauth/forgot-password', function () {
+    return view('log-in.forgotpassword');
 });
